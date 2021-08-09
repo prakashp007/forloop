6a9b1bcd54782277e9d257c3b373af015929a449
parmar <ppb5787@gmail.com>
2021-08-09 12:02:11 +0530

    aad some new fie

---
+++ src/com/prakashp007/Main.java
@@ -0,0 +1,20 @@
+package com.prakashp007;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //forloop in java
+        //syn..int i(initialization),i<100(condtion statement),i++/i--(inc/dec)
+        //1 to 100
+        for (int i=1;i<=100;i++)
+        {
+            System.out.println(i);
+        }
+        //100 to 1
+        for (int i=100;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
